c8d0da434fb33f0669178f7ec974ad48c5c183b8
MathiVathani3008 <mathivathanicr@gmail.com>
2025-07-28 18:28:04 +0530

    Moved React app into subfolder

--- React-App/src/main.jsx
+++ React-App/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import { CartProvider } from './Context/CartContext.jsx';
-import { AddressProvider } from './Context/AddressContext';
+import { AddressProvider } from './Context/AddressContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
